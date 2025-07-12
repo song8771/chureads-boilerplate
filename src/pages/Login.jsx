284f153351a693c8bf98c7e72e371408c363d2be
dasom222g <dasom228@gmail.com>
2025-07-12 21:56:28 +0900

    refactoring: Remove comments

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,28 +5,9 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   // logic
-  // const history = useNavigate();
-
-  // const goToHome = () => {
-  //   history("/");
-  // };
-
-  // 자식으로부터 받은 inputValue의 값을 state에 저장
-  /**
-   * 1. handleInputChange함수에서 data라는 inputValue와 field라는 해당 input의 필드값을 받아온다.
-   * 2. 사용자가 email필드를 입력하면 email state에 inputValue의 값을 넣어준다.
-   * 3. 사용자가 password필드를 입력하면 password state에 inputValue의 값을 넣어준다.
-   * 4. form태그에서 onSubmit이라는 이벤트를 handleLogin이라는 함수에 연결한다
-   * 5. handleLogin에서 email, password 의 값을 확인한다.
-   */
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  // const [formData, setFormData] = useState({
-  //   email: "",
-  //   password: "",
-  // });
 
   const handleInputChange = (inputValue, field) => {
     // field: 'email', 'password'
@@ -51,13 +32,6 @@ const Login = () => {
   return (
     <div className="h-full flex flex-col justify-center">
       <div className="text-center px-6">
-        {/* <h2>Login</h2> */}
-        {/* <button type="button" onClick={goToHome}>
-          Home화면으로 이동
-        </button> */}
-        {/* <Link to={"/"} style={{ color: "red" }} className="link">
-          Home화면으로 이동
-        </Link> */}
         <h1 className="flex justify-center">
           <img src="./images/logo.svg" alt="churead로고" />
         </h1>
